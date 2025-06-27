53172c1783a1ddd04ca0830c96596bd47e504c16
dhanu902 <dhanuji.20230759@iit.ac.lk>
2025-06-27 11:49:20 +0530

    link pages and made changes in ui

--- Frontend/src/Components/Footer.jsx
+++ Frontend/src/Components/Footer.jsx
@@ -1,24 +1,12 @@
 import React from "react";                          /*<------ import react library*/
+import './Footer.css';
 
 const Footer = () => {
     return(
-        <footer style={styles.footer}>
+        <footer className="footer">
             <p>©️ 2025 FoodieBot. All rights reserved.</p>
         </footer>  
     );
 };
-
-const styles = {
-    footer: {
-      backgroundColor: '#4B0082',
-      color: 'white',
-      textAlign: 'center',
-      padding: '0.75rem',
-      position: 'fixed',
-      width: '100%',
-      bottom: 0,
-    },
-};  
-
 
 export default Footer;
---
+++ Frontend/src/Pages/ChatPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ChatPage = () => {
+    return(
+        <div className='text-center'>
+            <h2>Chat with FoodieBot</h2>
+            <p>Type your request & get instant food suggestions..!</p>
+        </div>
+    );
+};
+
+export default ChatPage;
